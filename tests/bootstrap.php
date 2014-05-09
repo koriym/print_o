baf414e2dce1f25a8afa61d29c6b69a889bac750
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-05-10 03:37:11 +0900

    add test loader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,7 @@
 <?php
 
-require dirname(__DIR__)     . '/vendor/autoload.php';
+error_reporting(E_ALL);
+
+$loader = require dirname(__DIR__)     . '/vendor/autoload.php';
+/** @var $loader \Composer\Autoload\ClassLoader */
+$loader->addPsr4('Koriym\Printo\\', __DIR__);
