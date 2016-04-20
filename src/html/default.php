b25fa911afd6068d60f973a606e57424081391ff
phofmann-trust <phofmann@trustinternational.com>
2016-04-20 15:43:05 +0200

    Use protocol-relative URL for d3.min.js

--- src/html/default.php
+++ src/html/default.php
@@ -36,7 +36,7 @@ return <<<EOT
 
 </style>
 <body></body>
-<script src="http://koriym.github.io/print_o/d3/3.4.13/d3.min.js"></script>
+<script src="//koriym.github.io/print_o/d3/3.4.13/d3.min.js"></script>
 <script>
     var list = {$list};
     var width = window.innerWidth, height = window.innerHeight;
